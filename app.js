730445295c1c47a08efce3b498d6c6f734fadbb9
giuliarappo <rappogiulia@gmail.com>
2022-09-07 15:40:25 +0200

    mostra lista piatti completa

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 let menu;
 let categoria;
-let antipasto1;
-let strong1;
 let primi;
+let antipasti;
+let secondi;
+let contorni;
 
 function caricaMenu(ilFileJson) {
     $.getJSON(ilFileJson, function( JSON ) {
         menu = JSON.menu;
         categoria = Object.getOwnPropertyNames(JSON.menu);
         primi = menu.primi;
+        antipasti = menu.antipasti;
+        secondi = menu.secondi;
+        contorni = menu.contorni;
         console.log("menu importato!!");
         visualizzaMenu()
 
@@ -19,15 +23,41 @@ function caricaMenu(ilFileJson) {
 }
 
 function visualizzaMenu() {
+    //antipasti
+    for (let index = 0; index < antipasti.length; index++) {
+        let htmlElementPiatto = document.getElementsByClassName("antipasto")[index];
+        let htmlElementPrezzo = document.getElementsByClassName("prezzoAntipasto")[index];
+        htmlElementPiatto.innerText = antipasti[index].piatto;
+        htmlElementPrezzo.innerText = antipasti[index].prezzo;
+    }
+
+    //primi
     for (let index = 0; index < primi.length; index++) {
         console.log(primi[index]);
-        antipasto1 = document.getElementsByTagName("h4")[index];
-        strong1 = document.getElementsByTagName("strong")[index];
-        antipasto1.innerText = primi[index].piatto;
-        strong1.innerText = primi[index].prezzo;
+        let htmlElementPiatto = document.getElementsByClassName("primo")[index];
+        let htmlElementPrezzo = document.getElementsByClassName("prezzoPrimo")[index];
+        htmlElementPiatto.innerText = primi[index].piatto;
+        htmlElementPrezzo.innerText = primi[index].prezzo;
     }
-    
+
+    //secondi
+    for (let index = 0; index < secondi.length; index++) {
+        let htmlElementPiatto = document.getElementsByClassName("secondo")[index];
+        let htmlElementPrezzo = document.getElementsByClassName("prezzoSecondo")[index];
+        htmlElementPiatto.innerText = secondi[index].piatto;
+        htmlElementPrezzo.innerText = secondi[index].prezzo;
+    }
+
+    //contorni
+    for (let index = 0; index < contorni.length; index++) {
+        let htmlElementPiatto = document.getElementsByClassName("contorno")[index];
+        let htmlElementPrezzo = document.getElementsByClassName("prezzoContorno")[index];
+        htmlElementPiatto.innerText = contorni[index].piatto;
+        htmlElementPrezzo.innerText = contorni[index].prezzo;
+    }
 }
+
+
 
 $(document).ready(function () {
     caricaMenu("menu.json"); 
